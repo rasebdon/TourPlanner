0a1f9ac2e23439503524fc151272ab8fd471abac
Raphael Dohnalek <r.dohnalek@gmx.at>
2022-03-08 15:48:37 +0100

    Add controller base

---
+++ TourPlanner.BL.MapQuestAPI/MapQuestHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace TourPlanner.BL.MapQuestAPI
+{
+    public class MapQuestHandler
+    {
+        private readonly HttpClient _httpClient;
+
+        public MapQuestHandler()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public object? GetMap(List<object> routePoints)
+        {
+            return null;
+        }
+    }
+}
